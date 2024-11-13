02a8c23cafe70e9cf4fcc774f7126f26d5869114
Aymeric M <aymeric.martinache@gmail.com>
2024-11-13 15:36:38 +0100

    10.Introduction-aux-objets

--- 1.Les-bases-de-JavaScript/10.Introduction-aux-objets/script.js
+++ 1.Les-bases-de-JavaScript/10.Introduction-aux-objets/script.js
@@ -6,50 +6,54 @@
     On parle d'une collection de clés-valeurs. (keyed collection)
 
 */
-
+const store = {
+    customers: 278,
+    turnover: 451200,
+    employees: 3,
+    rent: 3500,
+};
 
 // On accède aux valeurs grâce à deux notations, la dot et la bracket notation
-
 // dot notation
-
+console.log('Nombre de clients : ', store.customers);
 
 // bracket notation
-
+console.log("Nombre d'employées : ", store['employees']);
 
 // On peut modifier la valeur d'une propriété
-
+store.turnover = 99;
+console.log('Nombre de turnover : ', store.turnover);
 
 // Imbrication d'objets
-
-// C'est assez complexe quand on débute de s'y retrouver, pour l'instant ne vous posez pas trop de question, mais appréciez juste la syntaxe, et tentez de retrouver des valeurs et d'exécuter des fonctions. 
+// C'est assez complexe quand on débute de s'y retrouver, pour l'instant ne vous posez pas trop de question, mais appréciez juste la syntaxe, et tentez de retrouver des valeurs et d'exécuter des fonctions.
 
 const player = {
-  spells: {
-    fire() {
-      return "fire";
+    spells: {
+        fire() {
+            return 'fire';
+        },
+        freeze: function () {
+            return 'freeze';
+        },
     },
-    freeze: function(){
-      return "freeze";
-    }
-  },
-  bag: [
-    {
-      id: 1,
-      name: "bow",
-      description: "deals 3 damage"
-    },
-    {
-      id: 2,
-      name: "shield",
-      description: "increases armor by 5"
-    },
-    {
-      id: 3,
-      name: "healing potion",
-      description: "heals 10 HP"
-    }
-  ]
-}
+    bag: [
+        {
+            id: 1,
+            name: 'bow',
+            description: 'deals 3 damage',
+        },
+        {
+            id: 2,
+            name: 'shield',
+            description: 'increases armor by 5',
+        },
+        {
+            id: 3,
+            name: 'healing potion',
+            description: 'heals 10 HP',
+        },
+    ],
+};
 console.log(player.spells.fire());
 console.log(player.bag[0].name);
-console.log(player.bag.find(element => element.name === "shield"));
+console.log(player.bag.find((element) => element.name === 'shield'));

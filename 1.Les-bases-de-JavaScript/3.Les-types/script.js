37853b54c31cd3bd60c6ed11a62d81847744d582
Aymeric M <aymeric.martinache@gmail.com>
2024-11-13 13:55:07 +0100

    Adding ReadMe

--- 1.Les-bases-de-JavaScript/3.Les-types/script.js
+++ 1.Les-bases-de-JavaScript/3.Les-types/script.js
@@ -6,48 +6,51 @@
     NUMBER : Le type nombre représente un nombre entier ou à virgule entre 9007199254740991 et 9007199254740992.
     Au-delà de ces nombres, les calculs ne sont plus fiables et nous pouvons utiliser un nouveau type bigInt.
 */
-
-
+let age = 99;
+console.log(age, '->', typeof age);
 
 /*  
     STRING : Le type représentant des chaînes de caractères.
     On peut utiliser la longueur de la chaîne grâce à length.
     On peut accéder à une lettre avec la bracket notation.
 */
-
-
+const userName = 'Paul';
+console.log(userName);
+console.log(userName, '->', typeof userName);
+console.log(userName.length, 'Nombre de caractères');
+console.log(userName[0]);
 
 /* 
     BOOLEAN : Un type représentant une valeur vraie(true) ou fausse(false).
 */
-
-
+let isAdult = false;
+console.log(isAdult);
+console.log('boolean ->', typeof isAdult);
 
 /* 
     UNDEFINED : Le type représentant une valeur non-définie, typiquement quand une variable n'a pas encore reçue de valeurs.
 */
-
-
+let price;
+console.log('Valeur non définie pour la variable -> ', price);
 
 /* 
     NULL : Ce type réprente une valeur qui n'existe pas, typiquement quand on essaye d'utliser une variable non créée. 
 */
-
-
+console.log("Une variable qui n'existe pas ->", null);
 
 /* 
     BIGINT : Ce type représente des nombres très grands.
     Il a été rajouté récemment pour palier à des problèmes assez rares.
 */
-
-
+console.log(
+    'BigInt 99999999999999999999999999999999 ->',
+    typeof 99999999999999999999999999999999n
+);
 
 /* 
     SYMBOL : Un symbole est un identifiant unique qui permet d’éviter des collisions entre des valeurs similaires.
     À ce stade de votre apprentissage, il ne vous sert à rien de vous pencher dessus.
 */
-
-
 
 /* 
     OBJECT : Un objet est un container de propriétés représentant des valeurs.
@@ -55,22 +58,16 @@
     C'est un type très important, qu'il faut maîtriser et qui représente une grande partie du fonctionnement du langage JavaScript.
 */
 
-
-
 /* 
     FUNCTION : Les fonctions sont ... des objets ! Même si typeof nous donne le "type" function, ce sont bien des objets éxécutables. 
     Nous verrons le fonctionnement des fonctions en détail dans ce cours, regardons simplement un exemple classique. 
 */
-
-
-
 
 /* 
     ARRAY : Les tableaux sont ... également des objets ! 
     Mais alors, pourquoi les avoir créés ? Afin d'exploiter certaines propriétés et méthodes très pratiques, propres aux tableaux, comme la propriété length, ou les méthodes .map(), .filter(), etc ...
     Nous reviendrons également très en détail sur les tableaux.
 */
-
 
 // On accède aux élément d'un tableau grâce à l'indice de ce dernier qui commence à 0
 // C'est à dire que le premier élément est à l'indice 0.
--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# Ecole_du_Web-Cours_JS
+# Cours de JavaScript - Notes et Exercices
+
+Bienvenue sur ce dépôt GitHub ! 👋  
+Ce projet contient mes notes et mes exercices issus du cours **JavaScript** que j'ai suivi sur [l'École du Web](https://www.ecole-du-web.net/). Ce cours couvre les bases de JavaScript, avec des exemples pratiques et des exercices pour bien comprendre chaque concept.
+
+## 📚 Contenu du cours
+
+Le cours est structuré comme suit :
+
+1. **Introduction à JavaScript**
+
+    - Créer un dossier et exécuter du JavaScript
+    - Déclarer des variables
+
+2. **Les Bases de JavaScript**
+
+    - Les différents types
+    - Les commentaires
+    - Les expressions et les instructions
+    - Les différents opérateurs
+
+3. **Manipulation des chaînes de caractères et des fonctions**
+
+    - Introduction aux chaînes de caractères
+    - Introduction aux fonctions
+    - Comprendre l'**hissage (hoisting)**
+
+4. **Travail avec les objets et tableaux**
+
+    - Introduction aux objets
+    - Introduction aux tableaux
+
+5. **Introduction au DOM et aux évènements**
+
+    - Introduction au DOM
+    - Introduction aux évènements
+
+6. **Exercices pratiques**
+    - Exercice 1 : Les variables
+    - Résolution de l'exercice : Les variables
+    - Introduction à l'exercice : Naming
+    - Résolution de l'exercice : Naming
+    - Introduction à l'exercice : Objets et tableaux
+    - Résolution de l'exercice : Objets et tableaux
+
+## 🛠️ Technologies
+
+Ce projet utilise :
+
+-   **JavaScript** : Langage de programmation principal utilisé dans ce cours.
+-   **Node.js** : Pour exécuter les scripts JavaScript localement.
+
+## 📖 Ressources supplémentaires
+
+-   [MDN JavaScript](https://developer.mozilla.org/fr/docs/Web/JavaScript) : Documentation complète sur JavaScript.
+-   [Node.js](https://nodejs.org/) : Pour exécuter du JavaScript côté serveur.
+
+---
+
+Merci d'avoir visité mon dépôt et bon apprentissage de JavaScript ! 🚀

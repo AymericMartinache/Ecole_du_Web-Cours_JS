e7b1658d16b988fc0c233bc4c3c96a2c9a4765ca
Aymeric M <aymeric.martinache@gmail.com>
2024-11-13 11:28:46 +0100

    2-Les-variables

--- 1.Les-bases-de-JavaScript/2.Les-variables/script.js
+++ 1.Les-bases-de-JavaScript/2.Les-variables/script.js
@@ -2,21 +2,20 @@
     Une variable est le nom d'un emplacement dans la mémoire de votre ordinateur permettant de stocker des valeurs et de les utiliser dans le code. 
 */
 
-
 /* let */
+let mainCaractere = 'Thomas';
+mainCaractere = 'Victore';
+console.log(mainCaractere);
 
+// On peut déclarer une variable sans lui assigner de valeur:
+let secondCaractere;
+console.log(secondCaractere); // Retourne undifined
 
 /* ⚠️Impossible d'utiliser let et const avant de les déclarer */
-
-
 /* ⚠️Impossible de redéclarer une let ou const avec le même nom */
 
-
 /* const */
-
-
-
-/* var */
-
+const userName = 'Ana8475';
+console.log(userName);
 
 /* ⚠️ Attention, on ne peut pas déclarer de constante sans les initialiser, contrairement aux var et let. */

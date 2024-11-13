97223d20ffec3c4e0e4f5a4995d5c463ad5c67c9
Aymeric M <aymeric.martinache@gmail.com>
2024-11-13 15:49:09 +0100

    11.Introduction aux tableaux

--- 1.Les-bases-de-JavaScript/11.Introduction-aux-tableaux/script.js
+++ 1.Les-bases-de-JavaScript/11.Introduction-aux-tableaux/script.js
@@ -14,31 +14,27 @@
     index         0   1   2
 */
 
-
-const users = ["Paul", "Ana", "Juliette", "Alexandre", "Victor"];
-
+const users = ['Paul', 'Ana', 'Juliette', 'Alexandre', 'Victor'];
 
 // propriété length très pratique
-
-
+console.log('Taille du tableau : ', users.length);
 
 // Pour sélectionner le dernier élément d'un tableau
+console.log('Dernier élément du tableau : ', users[users.length - 1]);
 
+// push() -> ajout d'un élément
+users.push('Aymeric');
+console.table(users);
 
+// pop() -> retire le dernier élément du tableau
+users.pop();
+console.table(users);
 
-// push()
-
-
-
-// pop()
-
-
-
-// indexOf()
-
+// indexOf() -> trouve l'index d'un élément
+console.log('Index de Anna dans le tableau : ', users.indexOf('Anna'));
 
 // Les tableaux sont des objets !
 
-console.log(typeof users);  // object
+console.log('Type du tableau : ', typeof users); // object
 
 // En effet, ils ont des propriétés (0,1,2,3,4,5...) et on y accède via la bracket notation.

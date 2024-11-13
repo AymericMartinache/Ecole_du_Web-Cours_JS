faeeaf43c982f5901ca19fd05e66a9f9c8b3fc27
Aymeric M <aymeric.martinache@gmail.com>
2024-11-13 14:14:33 +0100

    3.Les-types

--- 1.Les-bases-de-JavaScript/3.Les-types/script.js
+++ 1.Les-bases-de-JavaScript/3.Les-types/script.js
@@ -18,6 +18,7 @@ const userName = 'Paul';
 console.log(userName);
 console.log(userName, '->', typeof userName);
 console.log(userName.length, 'Nombre de caractères');
+// Lettre à l'index 0
 console.log(userName[0]);
 
 /* 
@@ -48,26 +49,45 @@ console.log(
 );
 
 /* 
-    SYMBOL : Un symbole est un identifiant unique qui permet d’éviter des collisions entre des valeurs similaires.
-    À ce stade de votre apprentissage, il ne vous sert à rien de vous pencher dessus.
-*/
-
-/* 
     OBJECT : Un objet est un container de propriétés représentant des valeurs.
     On appelle une propriété "méthode" lorsque sa valeur est une fonction.
     C'est un type très important, qu'il faut maîtriser et qui représente une grande partie du fonctionnement du langage JavaScript.
 */
+const normay = {
+    name: 'Norway',
+    capital: 'Oslo',
+    population: 5400000,
+    GDP: 482,
+};
+console.log("C'est un objet -> ", typeof normay, normay);
+console.log('Nom: ', normay.name);
+console.log('Capitale: ', normay.capital);
+console.log('GDP: ', normay.GDP);
 
 /* 
     FUNCTION : Les fonctions sont ... des objets ! Même si typeof nous donne le "type" function, ce sont bien des objets éxécutables. 
     Nous verrons le fonctionnement des fonctions en détail dans ce cours, regardons simplement un exemple classique. 
 */
+function getSquare(a) {
+    return a * a;
+}
+console.log('Le résultat est :', getSquare(4));
+console.log('Le type est :', typeof getSquare);
 
 /* 
     ARRAY : Les tableaux sont ... également des objets ! 
     Mais alors, pourquoi les avoir créés ? Afin d'exploiter certaines propriétés et méthodes très pratiques, propres aux tableaux, comme la propriété length, ou les méthodes .map(), .filter(), etc ...
     Nous reviendrons également très en détail sur les tableaux.
 */
+
+const salaries = [1580, 2000, 1610, 3400, 2745];
+console.log(salaries[0]);
+console.log('Le type est :', typeof salaries);
+
+// Exemple de méthode pour un tableau:
+salaries.forEach((salary) => {
+    console.log('La salaire est de : ', salary);
+});
 
 // On accède aux élément d'un tableau grâce à l'indice de ce dernier qui commence à 0
 // C'est à dire que le premier élément est à l'indice 0.

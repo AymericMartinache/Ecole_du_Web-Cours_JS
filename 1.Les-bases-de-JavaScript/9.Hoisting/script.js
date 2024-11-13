09583aee80c6230b15c5b7f4751090aff968853f
Aymeric M <aymeric.martinache@gmail.com>
2024-11-13 15:23:34 +0100

    9.Hoisting

--- 1.Les-bases-de-JavaScript/9.Hoisting/script.js
+++ 1.Les-bases-de-JavaScript/9.Hoisting/script.js
@@ -2,6 +2,7 @@
     1. Le "Hoisting"
 
     L'hissage, est la façon dont les variables déclarées avec var et les déclarations de fonction se font analyser en premier et "hisser" en haut du code lors de l'execution d'un fichier JS. 
+    
     Cela les rend donc accessibles à tout le reste du code, même si les variables et fonctions sont déclarées après leur utilisation.
 
     Les var retourneront néanmoins undefined, car elles sont analysées sans qu'on leur assigne de valeur...
@@ -11,3 +12,14 @@
     Attention, il n'y a pas de hoisting avec les let & const. 
 */
 
+/* Expression de fonction */
+const foo = function () {
+    console.log('foo');
+};
+foo();
+
+// Déclaration de fonction
+function bar() {
+    console.log('bar');
+}
+bar();

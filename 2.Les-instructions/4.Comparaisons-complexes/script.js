a1e800d51fea790f58cb2870d10910cb412e412a
Aymeric M <aymeric.martinache@gmail.com>
2024-11-14 17:49:22 +0100

    Comparaisons complexes

--- 2.Les-instructions/4.Comparaisons-complexes/script.js
+++ 2.Les-instructions/4.Comparaisons-complexes/script.js
@@ -3,17 +3,36 @@
 */
 
 const user1 = {
-  name: "Victor",
-  age: 35,
-  admin: true,
-  position: "CEO"
-}
+    name: 'Victor',
+    age: 35,
+    admin: true,
+    position: 'CEO',
+};
+
 const user2 = {
-  name: "Paul",
-  age: 25,
-  admin: false,
-  position: "Employee"
+    name: 'Paul',
+    age: 25,
+    admin: false,
+    position: 'Employee',
+};
+
+// Fonction qui verifier les permissions
+function checkPermission(user) {
+    if (
+        (user.age > 30 && user.admin && user.position === 'CEO') ||
+        user.position === 'Direction'
+    ) {
+        return 'Accès autorisé';
+    } else {
+        return 'Accès refusé';
+    }
 }
+console.log(checkPermission(user2));
 
-
-
+// Refactoring
+function checkPermission2(user) {
+    if (!user.admin || user.age < 30) return 'Accès refusé';
+    if (user.position === 'CEO' || user.position === 'Direction')
+        return 'Accès autorisé';
+}
+console.log(checkPermission2(user1));

c094f6e8b585bb7a93eb6c3f8a80d9a4647f81b3
Aymeric M <aymeric.martinache@gmail.com>
2024-11-18 18:33:42 +0100

    Exercices - Les fonctions

--- 3.Les-fonctions-dans-le-d‚tail/Exercices/1.Exercice-fonctions/script.js
+++ 3.Les-fonctions-dans-le-d‚tail/Exercices/1.Exercice-fonctions/script.js
@@ -9,7 +9,15 @@
     Écrivez cette fonction sous la forme d'une fonction fléchée puis commentez-là et écrivez-là sous la forme d'une fonction classique.  
 */
 
+// Fonction fléchée
+const dividedBy10 = (num) => num / 10;
+console.log(dividedBy10(100));
 
+// Fonction classique
+function dividedBy100(num) {
+    return num / 100;
+}
+console.log(dividedBy100(5000));
 
 /* 
     2. this weather
@@ -18,13 +26,14 @@
 
     Formule celsius - fahrenheit  :  (celsius * 1.8) + 32
 */
-
 const weather = {
-  city: "London",
-  temp: 16,
-}
-
-
+    city: 'London',
+    temp: 15,
+    getFahrenheit() {
+        return this.temp * 1.8 + 32;
+    },
+};
+console.log(weather.getFahrenheit());
 
 /* 
   3. Ordre supérieur.
@@ -35,26 +44,46 @@ const weather = {
 */
 
 const travelers = [
-{
-  name: "Jean",
-  nationality: "FR"
-},
-{
-  name: "Victor",
-  nationality: "FR"
-},
-{
-  name: "Paul",
-  nationality: "FR"
-},
-{
-  name: "Jack",
-  nationality: "EN"
-},
-{
-  name: "Giulia",
-  nationality: "IT"
-},
-]
+    {
+        name: 'Jean',
+        nationality: 'FR',
+    },
+    {
+        name: 'Victor',
+        nationality: 'FR',
+    },
+    {
+        name: 'Paul',
+        nationality: 'FR',
+    },
+    {
+        name: 'Jack',
+        nationality: 'EN',
+    },
+    {
+        name: 'Giulia',
+        nationality: 'IT',
+    },
+];
 
+function areFromNationality(arr, callback) {
+    for (const traveler of arr) {
+        if (!callback(traveler)) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(
+    areFromNationality(travelers, (traveler) => traveler.nationality === 'FR')
+);
+console.log(
+    areFromNationality(travelers, (traveler) => traveler.nationality === 'EN')
+);
 
+// On peut également utiliser la méthode JS "every" qui fait la même chose que notre fonction "areFromNationality"
+function areFromNationality2(arr, nationality) {
+    return arr.every((traveler) => traveler.nationality === nationality);
+}
+console.log(areFromNationality2(travelers, 'FR'));
+console.log(areFromNationality2(travelers, 'EN'));

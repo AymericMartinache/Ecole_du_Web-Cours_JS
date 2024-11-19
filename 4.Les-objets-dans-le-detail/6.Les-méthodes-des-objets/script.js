5fcf96fa9448be8a0fcfa72727d9b85db2018571
Aymeric M <aymeric.martinache@gmail.com>
2024-11-19 10:42:00 +0100

    6.Les-méthodes-des-objets

--- 4.Les-objets-dans-le-detail/6.Les-méthodes-des-objets/script.js
+++ 4.Les-objets-dans-le-detail/6.Les-méthodes-des-objets/script.js
@@ -3,51 +3,43 @@
 */
 
 const rocket = {
-  model: "Falcon Heavy",
-  price: 97,
-  weight: 549,
-  height: 70,
-  diameter: 3.7,
-  fuel: "Kerosene" 
-}
+    model: 'Falcon Heavy',
+    price: 97,
+    weight: 549,
+    height: 70,
+    diameter: 3.7,
+    fuel: 'Kerosene',
+};
 
 /* 
     1. hasOwnProperty()
 
     Retourne un booléen indiquant la présence d'une propriété.
 */
-
-
-
+console.log(rocket);
+console.log(rocket.hasOwnProperty('model'));
+console.log(rocket.hasOwnProperty('cat'));
 
 /* 
     2. Object.keys()
 
     Retourne un tableau contenant les propriétés d'un objet sous forme de chaînes de caractères.
 */
-
-
-
+console.log(Object.keys(rocket));
 
 /* 
     3. Object.values()
 
     Retourne un tableau contenant les valeurs d'un objet.
 */
-
-
-
-
+console.log(Object.values(rocket));
 
 /* 
     4. Object.entries()
 
     Retourne un tableau contenant les propriétés-valeurs d'un objet sous forme d'un tableau contenant des sous-tableaux.
 */
-
-
-
-
+console.log(Object.entries(rocket));
 
 /* 
     5. Object.freeze()
@@ -56,4 +48,6 @@ const rocket = {
     Attention, une fois geler, on ne peut plus le degeler.
 
 */
-
+Object.freeze(rocket);
+rocket.color = 'red';
+console.log(rocket);

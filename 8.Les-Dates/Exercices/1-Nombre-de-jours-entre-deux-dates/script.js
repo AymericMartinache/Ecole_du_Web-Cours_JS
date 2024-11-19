61ea0d8122b248663b05adb6bb699cc595418cbe
Aymeric M <aymeric.martinache@gmail.com>
2024-11-19 17:19:32 +0100

    Exercice - 1-Nombre-de-jours-entre-deux-dates

--- 8.Les-Dates/Exercices/1-Nombre-de-jours-entre-deux-dates/script.js
+++ 8.Les-Dates/Exercices/1-Nombre-de-jours-entre-deux-dates/script.js
@@ -2,12 +2,14 @@
     1. Nombre de jours entre deux dates.
 
     Créez une fonction daysBetweenDates(firstDate, secondDate) qui retourne le nombre de jours passés entre deux dates données.
-
     ex : daysBetweenDates(new Data(2021,0,13), new Date(2021,0,22)) // 9
 */
 
-function daysBetweenDates(firstDate, secondDate){
-
+function daysBetweenDates(firstDate, secondDate) {
+    const difference = secondDate.getTime() - firstDate.getTime();
+    return Math.round(difference / (1000 * 60 * 60 * 24));
 }
-console.log(daysBetweenDates(new Date(2021,0,13), new Date(2021,0,22)))
-console.log(daysBetweenDates(new Date(1021,0,13), new Date(2021,0,22)))
+console.log(
+    'Nombre de jours entre les dates : ',
+    daysBetweenDates(new Date(2021, 0, 13), new Date(2021, 0, 22))
+);

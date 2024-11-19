205c90051ce0173f5a9982f500aee9505b116e5a
Aymeric M <aymeric.martinache@gmail.com>
2024-11-19 14:25:14 +0100

    8-split-et-join

--- 5.Les-tableaux-dans-le-detail/8-split-et-join/script.js
+++ 5.Les-tableaux-dans-le-detail/8-split-et-join/script.js
@@ -1,15 +1,13 @@
 /* 
     1. .split(separator)
-
     Cette méthode permet de transformer une chaîne en tableau à partir d'un séparateur.
-
     Exemple : 
-
     "abc".split("") devient ["a","b","c"]
 */
 
-let string = "abc def ijk";
-
+let string = 'abc def ijk';
+const split = string.split(' ');
+console.log('Tableau splité : ', split);
 
 /* 
     2. .join(separator)
@@ -17,4 +15,6 @@ let string = "abc def ijk";
     Celle-ci fait l'inverse de .split(), elle transforme un tableau en chaîne.
 */
 
-let arr = ["a","b","c"];
+let arr = ['a', 'b', 'c'];
+const join = arr.join(' ');
+console.log('Chaîne joinée : ', join);

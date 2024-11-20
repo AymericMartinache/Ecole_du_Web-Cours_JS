31f50380ced3c8b2134210ef513a363c0d52737d
Aymeric M <aymeric.martinache@gmail.com>
2024-11-20 15:45:57 +0100

    2.Selectionner-les-parents-et-les-enfants

--- 9.Le-Document-Object-Model/2.Selectionner-les-parents-et-les-enfants/script.js
+++ 9.Le-Document-Object-Model/2.Selectionner-les-parents-et-les-enfants/script.js
@@ -18,11 +18,46 @@
             ...
         ]
     }
-    // 🔼 Ceci est une image pour vous aider à comprendre, ça ne représente pas les propriétés exactes utilisées.
+    🔼 Ceci est une image pour vous aider à comprendre, ça ne représente pas les propriétés exactes utilisées.
 
     Découvrons les propriétés pratiques liées aux relations parents / enfants.
 */
+const card = document.querySelector('.card');
+console.log('Child Nodes : ', card.childNodes);
+console.log('Children : ', card.children);
 
+// Premier noeud
+console.log('firstChild : ', card.firstChild);
 
-    
-    
+// Premeier élément HTML
+console.log('firstElementChild : ', card.firstElementChild);
+
+// Dernier noeud
+console.log('lastChild : ', card.lastChild);
+
+// Dernier élément HTML
+console.log('lastElementChild : ', card.lastElementChild);
+
+const title = document.querySelector('h2');
+console.log('Title : ', title);
+
+// Noeud précédent
+console.log('Title previousSibling : ', title.previousSibling);
+
+// Noeud suivant
+console.log('Title nextSibling : ', title.nextSibling);
+
+// Elément précédent
+console.log('Title previousElementSibling : ', title.previousElementSibling);
+
+// Elément suivant
+console.log('Title nextElementSibling : ', title.nextElementSibling);
+
+// Noeud parent
+console.log('Title parentNode : ', title.parentNode);
+
+// Nom du noeud
+console.log('Title nodeName : ', title.nodeName);
+
+// Elément parent
+console.log('Title parentElement : ', title.parentElement);

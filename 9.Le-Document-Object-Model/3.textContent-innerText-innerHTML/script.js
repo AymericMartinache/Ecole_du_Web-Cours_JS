7be7e12f8456256a2e0a4e0bdcfeca93ad99fe50
Aymeric M <aymeric.martinache@gmail.com>
2024-11-20 16:06:49 +0100

    3.textContent-innerText-innerHTML

--- 9.Le-Document-Object-Model/3.textContent-innerText-innerHTML/script.js
+++ 9.Le-Document-Object-Model/3.textContent-innerText-innerHTML/script.js
@@ -2,47 +2,50 @@
     Ces trois propriétés pratiques se ressemblent, mais il faut bien comprendre leurs différences.
 */
 
+//* Sélection du container
+const container = document.querySelector('.container');
 
+//* Sélection du titre
+const title = document.querySelector('h2');
+
+// console.log(container, title);
+
+//* Element : innerHTML = string
 /* 
-    Element : innerHTML = string
-
     Transforme la chaîne de caractères fournie en éléments HTML si la syntaxe est respectée.
-    Rajoute du texte au contenu d'un élément si la chaîne fournie n'est pas au format HTML.
-    
-    Cette propriété comporte des risques si elle est mal utilisée.
-    Nous en parlerons un peu plus tard.
+    Rajoute du texte au contenu d'un élément si la chaîne fournie n'est pas au format HTML. 
 */
+//!    Cette propriété comporte des risques si elle est mal utilisée.N
 
-    // Rajoute un élément HMTL dans l'élément container.
+//* Rajoute un élément HMTL dans l'élément container.
+// container.innerHTML = "<input type='text' />";
 
-    // Retourne le contenu interne de container.
+//* Retourne le contenu interne de container.
+// container.innerHTML = 'Nouveau texte !';
 
+//* Element : textContent
 /* 
-    Element : textContent
-
     Retourne ou modifie le texte à l'intérieur d'un élément.
     Retournera également le contenu en "visibility: hidden;", contrairement à innerText.
 */
+//* Retourne le texte à l'intérieur de container, caché ou pas.
+// console.log('textContent du container : ', container.textContent);
 
-    // Retourne le texte à l'intérieur de container, caché ou pas.
-    // Change le texte à l'intérieur de container
+//* Change le texte à l'intérieur de container
+container.textContent = 'Nouveau texte ! 😁';
 
-
+//* Element : innerText
 /* 
-    Element : innerText
-    
     Retourne ou modifie le texte à l'intérieur d'un élément.
     Retournera seulement le texte visible, non-dissimulé par visibility: hidden.
 */
 
-    // Retourne seulement le texte visible
-    
+// Retourne seulement le texte visible
+console.log('innerText du container : ', container.innerText);
 
-
+//! De la dangerosité d'innerHTML.
 /* 
-    De la dangerosité d'innerHTML.
-
-    Il ne faut jamais transformer en HTML des données qu'on reçoit d'une source non-sûre.
+     Il ne faut jamais transformer en HTML des données qu'on reçoit d'une source non-sûre.
 
     Par prévention, HTML n'exécute pas un "<script></script>"" avec innerHTML.
 
@@ -53,13 +56,9 @@
     `<img src='1' onerror='alert("Error loading image")'>`;
 */
 
-
+//* textContent vs innerText
 /* 
-    textContent vs innerText
-
     Une petite guerre existe pour savoir s'il faut utiliser textContent ou innerText.
     Il y a certes, certaines différences entre les deux, mais vous pouvez la plupart du temps les utiliser de manière interchangeable.
     Je vous conseille d'utiliser textContent qui est en général plus acceptée.
 */
-
-

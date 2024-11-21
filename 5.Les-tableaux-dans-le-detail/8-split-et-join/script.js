10260d79df780f878290efff47ee724c58f9522e
Aymeric M <aymeric.martinache@gmail.com>
2024-11-21 10:43:39 +0100

    8.Creer-un-fragment

--- 5.Les-tableaux-dans-le-detail/8-split-et-join/script.js
+++ 5.Les-tableaux-dans-le-detail/8-split-et-join/script.js
@@ -3,20 +3,38 @@
     Cette méthode permet de transformer une chaîne en tableau à partir d'un séparateur.
     Exemple : 
     "abc".split("") devient ["a","b","c"]
+    
+    Explication ligne par ligne :
 */
 
-let string = 'abc def ijk';
-const split = string.split(' ');
-console.log('Tableau splité : ', split);
+// On déclare une variable str qui contient la chaîne 'abc'
+let str = 'abc';
+// On utilise split('') pour séparer chaque caractère et créer un tableau
+const split = str.split('');
+// On affiche le tableau créé qui sera ['a','b','c']
+console.log('Tableau créé : ', split);
+
+// On déclare une variable str2 qui contient la chaîne 'a-b-c'
+let str2 = 'a-b-c';
+// On utilise split('-') pour séparer la chaîne aux tirets et créer un tableau
+const split2 = str2.split('-');
+// On affiche le tableau créé qui sera ['a','b','c']
+console.log('Tableau créé : ', split2);
 
 /* 
     2. .join(separator)
-
     Celle-ci fait l'inverse de .split(), elle transforme un tableau en chaîne.
+    
+    Explication ligne par ligne :
 */
 
+// On déclare un tableau arr contenant trois éléments
 let arr = ['a', 'b', 'c'];
+// On utilise join('-') pour joindre les éléments avec des tirets
 const join = arr.join('-');
+// On affiche la chaîne créée qui sera 'a-b-c'
 console.log('Chaîne concaténée : ', join);
+// On utilise join('+') pour joindre les éléments avec des plus
 const join2 = arr.join('+');
+// On affiche la chaîne créée qui sera 'a+b+c'
 console.log('Chaîne concaténée : ', join2);
--- 9.Le-Document-Object-Model/8.Creer-un-fragment/script.js
+++ 9.Le-Document-Object-Model/8.Creer-un-fragment/script.js
@@ -3,5 +3,22 @@
 
     Les fragments permettent de rajouter du contenu sans créer un container pour rien.
     Les éléments contenus dans le fragment se rajoutent tout simplement dans l'élément souhaité. 
-
 */
+
+const title1 = 'A random title H2';
+const title2 = 'Another one H3';
+
+// Création du fragment
+const fragment = document.createDocumentFragment();
+
+// Création des éléments h2 et h3
+const h2 = document.createElement('h2');
+h2.textContent = title1;
+fragment.appendChild(h2);
+
+const h3 = document.createElement('h3');
+h3.textContent = title1;
+fragment.appendChild(h3);
+
+// Ajout du fragment dans le body
+document.body.appendChild(fragment);

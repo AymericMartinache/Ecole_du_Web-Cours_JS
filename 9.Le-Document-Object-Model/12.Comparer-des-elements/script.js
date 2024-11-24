c73226b0ae63d557e120f8cd463a5a50473072cb
Aymeric M <aymeric.martinache@gmail.com>
2024-11-24 22:34:09 +0100

    12.Comparer-des-elements

--- 9.Le-Document-Object-Model/12.Comparer-des-elements/script.js
+++ 9.Le-Document-Object-Model/12.Comparer-des-elements/script.js
@@ -6,3 +6,9 @@
     Si deux objets ont la même référence, ils représentent le même objet dans la mémoire.
 */
 
+const list = document.querySelector('.list');
+const item2 = document.querySelector('#item-2');
+const item3 = document.querySelector('.item-2');
+
+console.log(list === item2);
+console.log(item2 === item3);

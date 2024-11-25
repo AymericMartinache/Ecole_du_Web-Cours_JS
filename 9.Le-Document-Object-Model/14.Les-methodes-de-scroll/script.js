4780bc07a185af4e2b56e649cb3395fc8f32acdb
Aymeric M <aymeric.martinache@gmail.com>
2024-11-25 09:01:18 +0100

    14.Les-methodes-de-scroll

--- 9.Le-Document-Object-Model/14.Les-methodes-de-scroll/script.js
+++ 9.Le-Document-Object-Model/14.Les-methodes-de-scroll/script.js
@@ -4,45 +4,51 @@
 
 */
 
+const floatingButton = document.querySelector('.scroll-btn');
+floatingButton.addEventListener('click', handleScroll);
 
-
+//* document.documentElement.scrollTop
 /* 
-    document.documentElement.scrollTop
-
     Permet de se déplacer à un nombre X de pixels depuis le haut du document.
 */
-  
+// function handleScroll() {
+//     document.documentElement.scrollTop = 1000;
+// }
 
+//* Window.scrollTo(x,y || options{top, left, behavior})
 /* 
-    Window.scrollTo(x,y || options{top, left, behavior})
-    Exactemeent la même méthode que Window.scroll() ...
+    Exactement la même méthode que Window.scroll() ...
 
     Scroll jusqu'à un certain endroit défini par les paramètres.
     Si on passe deux arguments, cela correspond aux coordonnées x,y.
     Si l'on passe un objet, alors on peut définir les propriétés top, left et behavior.
 */
 
+// function handleScroll() {
+//     // window.scrollTo(0, 100);
+//     window.scrollTo({
+//         top: 100,
+//         behavior: 'smooth',
+//     });
+// }
 
-
-
+//* Element.scrollIntoView(alignToTop || options)
 /* 
-    Element.scrollIntoView(alignToTop || options)
-
     Scroll jusqu'à l'élément appelant.
     
     Le premier paramètre aligne le haut de l'élément avec le haut du viewport s'il est sur true, qui est la valeur par défaut.
 
     Au contraire, il alignera le bas de l'élément avec le bas du 
     viewport si le paramètre est sur false.
-    
 */
+// function handleScroll() {
+//     document.querySelector('h2').scrollIntoView(true);
+// }
 
-
-
-
+//* Window.scrollby(x,y || options)
 /* 
-    Window.scrollby(x,y || options)
-
     Permet cette fois-ci de scroller par montant de pixels.
 */
-
+function handleScroll() {
+    window.scrollBy(0, 100);
+}

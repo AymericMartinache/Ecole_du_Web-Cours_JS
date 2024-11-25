43b841649ea05b28e9a16997a3101ece416a35ab
Aymeric M <aymeric.martinache@gmail.com>
2024-11-25 17:09:01 +0100

    8.Les-evenements-de-clavier

--- 10.Les-evenements/8.Les-evenements-de-clavier/script.js
+++ 10.Les-evenements/8.Les-evenements-de-clavier/script.js
@@ -1,3 +1,17 @@
 /* 
     Les évènements de clavier peuvent nous permettre de connaître les touches pressées, ce qui peut être très utile dans de nombreux cas.
 */
+
+const keyboardInfo = document.querySelector('.keyboard-info');
+
+//* Appuie sur une touche du clavier
+// document.addEventListener('keydown', handleKey);
+
+//* Relache une touche duclavier
+document.addEventListener('keyup', handleKey);
+
+//* Fonction handleKey
+function handleKey(event) {
+    console.log(event.key);
+    keyboardInfo.textContent = `${event.key}`;
+}

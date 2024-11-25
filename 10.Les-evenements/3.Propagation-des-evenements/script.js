d2df8e65e18142dbec79abebbfd7634a210a31a1
Aymeric M <aymeric.martinache@gmail.com>
2024-11-25 15:32:41 +0100

    3.Propagation-des-evenements

--- 10.Les-evenements/3.Propagation-des-evenements/script.js
+++ 10.Les-evenements/3.Propagation-des-evenements/script.js
@@ -8,9 +8,21 @@
     S'il déclenche un autre écouteur d'évènement sur son passage, l'objet d'évènement qui lui sera passé représentera la cible qui a reçu cet évènement.
 */
 
+const container1 = document.querySelector('.container-1');
+const container2 = document.querySelector('.container-2');
+const container3 = document.querySelector('.container-3');
 
-
-
-
-
-
+container1.addEventListener('click', handleClick1);
+function handleClick1(event) {
+    console.log('CONTAINER1');
+}
+container2.addEventListener('click', handleClick2);
+function handleClick2(event) {
+    console.log('container2');
+}
+container3.addEventListener('click', handleClick3);
+function handleClick3(event) {
+    //! pour ne pas propoager les événements, on utilise "stopPropagation"
+    event.stopPropagation();
+    console.log('CONTAINER3');
+}

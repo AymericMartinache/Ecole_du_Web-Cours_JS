453c5884fc84e85c92cc223ef85df766c9362a31
Aymeric M <aymeric.martinache@gmail.com>
2024-11-26 09:30:38 +0100

    11.Les-evenements-au-toucher

--- 10.Les-evenements/11.Les-evenements-au-toucher/script.js
+++ 10.Les-evenements/11.Les-evenements-au-toucher/script.js
@@ -2,3 +2,6 @@
     Les "touch events" permettent de prendre en compte le "clic" d'un doigt sur un écran tactile.
 */
 
+document.addEventListener('touchstart', () => console.log('Touch start'));
+document.addEventListener('touchend', () => console.log('Touch end'));
+document.addEventListener('touchmove', () => console.log('Touch move'));

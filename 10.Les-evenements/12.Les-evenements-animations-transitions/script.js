ebba36e4674c198652474a1f8c6e212cb82133dc
Aymeric M <aymeric.martinache@gmail.com>
2024-11-26 15:19:57 +0100

    12.Les-evenements-animations-transitions

--- 10.Les-evenements/12.Les-evenements-animations-transitions/script.js
+++ 10.Les-evenements/12.Les-evenements-animations-transitions/script.js
@@ -6,3 +6,13 @@
     Animation : animationstart, animationend
 */
 
+const box = document.querySelector('.box');
+
+//* Transition
+box.addEventListener('transitionstart', () => console.log('Transition start'));
+box.addEventListener('transitionend', () => console.log('Transition end'));
+box.addEventListener('transitionrun', () => console.log('Transition run'));
+
+//* Animation
+box.addEventListener('animationstart', () => console.log('Animation start'));
+box.addEventListener('animationend', () => console.log('Animation end'));

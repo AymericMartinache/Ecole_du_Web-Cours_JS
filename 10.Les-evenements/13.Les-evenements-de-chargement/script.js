ff47c5b877e03ca7431a7eb55399ca91845845be
Aymeric M <aymeric.martinache@gmail.com>
2024-11-26 15:29:32 +0100

    13.Les-evenements-de-chargement

--- 10.Les-evenements/13.Les-evenements-de-chargement/script.js
+++ 10.Les-evenements/13.Les-evenements-de-chargement/script.js
@@ -1,3 +1,13 @@
 /*
     Voici les évènements concernant le chargement d'une page : DOMContentLoaded, load.
 */
+
+//* DOM Content Loaded
+//! Se fait sur document
+document.addEventListener('DOMContentLoaded', () =>
+    console.log('Construction du DOM terminée')
+);
+
+//* Load
+//! Se fait sur window
+window.addEventListener('load', () => console.log('Tout est chargé !'));

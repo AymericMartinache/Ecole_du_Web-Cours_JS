5855b3ceee2262ee600b3db98c03e8f11a2efd19
Aymeric M <aymeric.martinache@gmail.com>
2024-12-04 10:58:11 +0100

    6.Getter-setter

--- 13.Le-JavaScript-moderne/6.Getter-setter/script.js
+++ 13.Le-JavaScript-moderne/6.Getter-setter/script.js
@@ -4,7 +4,27 @@
     Sous le capot, c'est comme si une fonction était exécutée.
 */
 const fruitsShop = {
-    shopName: "Fruits Paradise",
-    fruits: ["🥭","🍇","🥥"]
-}
+    shopName: 'Fruits Paradise',
+    fruits: ['🥭', '🍇', '🥥'],
 
+    get fruitsLenght() {
+        console.log('Getter');
+
+        if (this.fruits.length <= 3) {
+            console.log('Warning ! We are running out of fruits');
+        }
+        return this.fruits.length;
+    },
+
+    set setName(value) {
+        if (!/fruits/i.test(value)) {
+            console.log('Name must contain fruits');
+            return;
+        }
+        this.shopName = value;
+    },
+};
+fruitsShop.fruitsLenght;
+
+fruitsShop.setName = 'Fruits heaven';
+console.log(fruitsShop);

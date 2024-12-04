9c722340ea2bec9ae9731fe6c20775de0d62678f
Aymeric M <aymeric.martinache@gmail.com>
2024-12-04 09:59:02 +0100

    3.Les-template-literals

--- 13.Le-JavaScript-moderne/3.Les-template-literals/script.js
+++ 13.Le-JavaScript-moderne/3.Les-template-literals/script.js
@@ -10,21 +10,26 @@
   Pour intégrer une expression, on utilise la syntaxe ${expression} entre les backticks.
 */
 
-/* Ajout d'expression */
-
+//* Ajout d'expression */
 const a = 5;
 const b = 10;
 
+// Ancienne méthode
+console.log("L'addition de a + b est égale : ", a + b);
 
-/* Saut de ligne */
+// Templat literal
+console.log(`L'addition de a + b est égale :  ${a + b}`);
 
-// console.log("Voici du texte \n qui passe à la ligne");
-// console.log(`Voici du texte 
-// qui passe à la ligne`);
+//* Saut de ligne */
+// Ancienne méthode
+console.log('Voici du texte \n qui passe à la ligne');
 
+// Templat literal
+console.log(`Voici du texte
+qui passe à la ligne`);
 
-/* Utilisation de l'expression retournée par une fonction. */
-function foo(){
-  return "abc";
+//* Utilisation de l'expression retournée par une fonction. */
+function foo() {
+    return 'abc';
 }
-// console.log(`L'alphabet commence par : ${foo()}`);
+console.log(`L'alphabet commence par : ${foo()}`);

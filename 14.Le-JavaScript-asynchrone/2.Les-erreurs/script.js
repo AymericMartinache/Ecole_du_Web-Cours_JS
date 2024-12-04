1fc748fe33a22d3103f0b2d810a0fb8959268554
Aymeric M <aymeric.martinache@gmail.com>
2024-12-04 21:01:26 +0100

    2.Les-erreurs

--- 14.Le-JavaScript-asynchrone/2.Les-erreurs/script.js
+++ 14.Le-JavaScript-asynchrone/2.Les-erreurs/script.js
@@ -11,28 +11,38 @@
   throw new Error(*expression*) // Similaire à throw Error() mais étant appelée sous la form d'un constructeur
 */
 
+// function foo() {
+//     console.log('FOO');
+//     // throw "Message d'erreur";
+//     throw new Error('Nouveau message d&aops;erreur');
+// }
+// foo();
 
+// console.log('Après Foo');
+// console.log('Après Foo');
+// console.log('Après Foo');
 
 /*
     Il existe une série d'instructions permettant de tester du code et de gérer les erreurs si elles ont lieu.
     C'est le try...catch...finally
-    Si une erreur a lieu dans le bloc try{}, elle déclenchera l'exécution du bloc catch{} et n'anulera pas l'exécution du reste du script.
+    Si une erreur a lieu dans le bloc try{}, elle déclenchera l'exécution du bloc catch{} et n'annulera pas l'exécution du reste du script.
 
     On pourra également récupérer les données de l'erreur.
  */
 
 try {
-
-}
-catch(e) {
-  
-}
-finally{
-
+    let a = 555;
+    if (typeof a !== 'string') {
+        throw Error('Erreur 404');
+    }
+} catch (e) {
+    console.dir(e);
+} finally {
+    console.log('Je suis exécuté quoi qu&apos;il arrive');
 }
 
 // Les instructions try catch permettent de ne pas bloquer l'exécution du reste du code s'il y a une erreur.
-// console.log("After try catch")
-// console.log("After try catch")
-// console.log("After try catch")
-// console.log("After try catch")
+console.log('After try catch');
+console.log('After try catch');
+console.log('After try catch');
+console.log('After try catch');

f1bab853936aaf2d51386b8b6cc850d5023e6adc
Aymeric M <aymeric.martinache@gmail.com>
2024-12-04 11:21:50 +0100

    8.Les-modules-ES6

---
+++ 13.Le-JavaScript-moderne/8.Les-modules-ES6/module1.js
@@ -0,0 +1,8 @@
+const userName = 'John';
+const age = 25;
+
+//* Export par défaut
+// export default userName;
+
+//* Export nommé
+export { userName, age };
--- 13.Le-JavaScript-moderne/8.Les-modules-ES6/script.js
+++ 13.Le-JavaScript-moderne/8.Les-modules-ES6/script.js
@@ -4,3 +4,12 @@
     Avant, on devait mettre des <script> les uns au-dessus des autres, mais c'était difficilement maintenable.
 */
 
+// import { userName, age } from './module1.js';
+
+//? on peut renommer les imports avec "as"
+import { userName as name, age } from './module1.js';
+console.log(name, age);
+
+import { add, multiply } from './module2.js';
+console.log(add(2, 3));
+console.log(multiply(2, 3));

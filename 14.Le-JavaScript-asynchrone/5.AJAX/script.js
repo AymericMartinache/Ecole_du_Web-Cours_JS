81546b72abd6209e263cf27011bad5a14bca9afb
Aymeric M <aymeric.martinache@gmail.com>
2024-12-05 17:04:45 +0100

    5.AJAX

--- 14.Le-JavaScript-asynchrone/5.AJAX/script.js
+++ 14.Le-JavaScript-asynchrone/5.AJAX/script.js
@@ -10,3 +10,26 @@
     Voici l'ancienne façon de faire, on utilise aujourd'hui la méthode fetch() qui est plus simple à utiliser mais qui utilise XMLHttpRequest sous le capot.
 */
 
+// Utilisation de l'api The cat API (https://api.thecatapi.com/v1/images/search)
+
+function getCatImage(url, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', handleLoad);
+    function handleLoad() {
+        callback(xhr.response);
+    }
+    xhr.send();
+}
+
+getCatImage('https://api.thecatapi.com/v1/images/search', (data) => {
+    console.log(data);
+
+    // Ajout de l'image sur la page
+    const container = document.querySelector('.image-container');
+    const img = document.createElement('img');
+    img.src = data[0].url;
+    container.appendChild(img);
+});

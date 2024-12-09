2e70c8c4ae4889d0b06531275c6d8b6a487b91e4
Aymeric M <aymeric.martinache@gmail.com>
2024-12-09 09:33:50 +0100

    6.La-methode-fetch - part 2

--- 14.Le-JavaScript-asynchrone/6.La-methode-fetch/script.js
+++ 14.Le-JavaScript-asynchrone/6.La-methode-fetch/script.js
@@ -1,19 +1,41 @@
 /* Découvrons la méthode fetch en JavaScript */
 
 // Fonction qui va fetch les données
-function getCatImage(url) {
-    fetch(url)
-        .then((response) => {
-            console.log(response);
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
-            createImg(data);
-        });
-}
+// function getCatImage(url) {
+//     fetch(url)
+//     .then((response) => {
+//         console.log(response);
+//         return response.json();
+//     })
+//     .then((data) => {
+//         console.log(data);
+//         createImg(data);
+//     });
+// }
+
+//? On est pas obligé de mettre le fetch dans une fonction
+fetch('https://api.thecatapi.com/v1/images/srch')
+    .then((response) => {
+        if (!response.ok) {
+            throw Error(`${response.status}`);
+        }
+        console.log(response);
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+        createImg(data);
+
+        // Gestion des erreurs
+    })
+    .catch((error) => {
+        // Sélection du message d'erreur
+        document.querySelector('.error-msg').textContent = `Error ! : ${error}`;
+        console.dir(error);
+    });
+
 // Appel de la fonction en ajoutant l'url de l'api
-getCatImage('https://api.thecatapi.com/v1/images/search');
+// getCatImage('https://api.thecatapi.com/v1/images/search');
 
 // Création de l'image
 function createImg(imgData) {

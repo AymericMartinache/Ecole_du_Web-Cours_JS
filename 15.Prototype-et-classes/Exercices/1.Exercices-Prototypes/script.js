c9f95795c2b253376da1c8318c178515677b08da
Aymeric M <aymeric.martinache@gmail.com>
2024-12-10 17:57:32 +0100

    1.Exercices-Prototypes

--- 15.Prototype-et-classes/Exercices/1.Exercices-Prototypes/script.js
+++ 15.Prototype-et-classes/Exercices/1.Exercices-Prototypes/script.js
@@ -2,37 +2,43 @@
     1. Que va retourner honk() dans la console ?
 */
 
-function Car(model, price){
-  this.model = model 
-  this.price = price 
+function Car(model, price) {
+    this.model = model;
+    this.price = price;
 }
-Car.prototype.honk = function(){
-  console.log("Honk Honk")
-}
+Car.prototype.honk = function () {
+    console.log('Honk Honk');
+};
 
-const fiat = new Car("500", 20000)
+const fiat = new Car('500', 20000);
 
-Car.prototype = {}
+Car.prototype = {};
 
-fiat.honk() // ?
+fiat.honk(); // Retourne bien la méthode honk -> log "Honk honk" car l'instance est faite AVANT le définition du nouveau prototype
 
+//* Les nouvelles instances  n'auront pas accès à cette méthode.
+const fiat2 = new Car('Doblo', 12000);
+// console.log(fiat2);
 
 /*
-    2. Voici deux fonctions constructeur, faites en sorte que Piano hérite du prototype d'Instrument.
-
-    Puis créez une constante 'yamaha' contenant un nouvel objet piano.
-    
-    Enfin, exécutez play() à partir de yamaha afin de vérifier que l'héritage fonctionne correctement.
+    2. Voici deux fonctions constructeur,   
 */
 
-function Instrument(){
+function Instrument() {}
+Instrument.prototype.play = function () {
+    console.log('Play music');
+};
 
-}
-Instrument.prototype.play = function(){
-  console.log("Play music")
+function Piano(type, price) {
+    this.type = type;
+    this.price = price;
 }
 
-function Piano(type, price){
-  this.type = type;
-  this.price = price;
-}
+//* Faites en sorte que Piano hérite du prototype d'Instrument.
+Object.setPrototypeOf(Piano.prototype, Instrument.prototype);
+
+//* Puis créez une constante 'yamaha' contenant un nouvel objet piano.
+const yamaha = new Piano('Long', 15000);
+
+//* Enfin, exécutez play() à partir de yamaha afin de vérifier que l'héritage fonctionne correctement.
+yamaha.play();

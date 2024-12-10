2d190ea49f232372344862cbbe8cb3ee7d3e48f7
Aymeric M <aymeric.martinache@gmail.com>
2024-12-10 16:15:24 +0100

    3.Les-Classes

--- 15.Prototype-et-classes/3.Les-Classes/script.js
+++ 15.Prototype-et-classes/3.Les-Classes/script.js
@@ -6,3 +6,35 @@
     Le terme "sucre syntaxique" est beaucoup utilisé pour définir ce genre d'ajout. 
 */
 
+//* Utilisation d'une classe
+class Planet {
+    constructor(name, radius, color) {
+        this.name = name;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    //* Ajout d'une méthode
+    spin() {
+        console.log('Spinning ! 🪐');
+    }
+}
+// console.log(typeof Planet);
+
+//* Création d'un objet Planet
+const mars = new Planet('Mars', 3400, 'red');
+console.log(mars);
+
+//! Equivaux à
+
+function Planet2(name, radius, color) {
+    this.name = name;
+    this.radius = radius;
+    this.color = color;
+}
+Planet2.prototype.spin = function () {
+    console.log('Spinning around');
+};
+
+const earth = new Planet2('Earth', 2600, 'blue', 1400);
+console.log(earth, mars);
